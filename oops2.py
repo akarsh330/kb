3c161dbef157f33208642f3164a76c4c3d84b3f5
akarsh330 <akarshgowda7711@gmail.com>
2025-01-29 23:08:01 +0530

    Create oops2.py

---
+++ oops2.py
@@ -0,0 +1,28 @@
+class maths:
+    def __init__(self):
+        self.sub_name='maths'
+    def add(self):
+        a,b=10,20
+        print(a+b)
+m1=maths()
+m1.add()
+
+
+
+
+
+
+
+class instagaram:
+    def _init_(self):
+        self.version='v32'
+        self.developer='xyz'
+    def authentication(self):
+        user_name=input('enter user namre :')
+        password=input('enter your password :')
+        if user_name=='prince123' and password=='1234':
+            print('login succesfull')
+        else:
+            print('login failed')
+m1=instagaram()
+m1.authentication()
