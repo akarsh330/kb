ab7676a68de1d8ca6840456c45b5c4a87f1a303c
akarsh330 <akarshgowda7711@gmail.com>
2025-01-29 23:07:44 +0530

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+for x in range(4):
+    for y in range(3):
+        print(f'({x},{y})')
+
+
+
+
+number=[1,1,1,1,5]
+for x in number:
+    output=''
+    for y in range(x):
+        output+='x'
+    print(output)
+
+
+
+
+numbers=[2,4,9,4,5,3]
+print(numbers[0:-1])
