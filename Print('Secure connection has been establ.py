5dcb5eb8a613d5092d4f91d7002ab66eef7ea3de
akarsh330 <akarshgowda7711@gmail.com>
2025-02-17 16:34:56 +0530

    Create Print('Secure connection has been establ.py

---
+++ Print('Secure connection has been establ.py
@@ -0,0 +1,10 @@
+print('Secure connection has been established to bank server')
+try:
+    P= int(input('Enter your principal amount'))
+    t=int(input('Enter the duration'))
+    r=10
+    si = (ptr)/100
+    print('Simple interest=',si)
+except:
+       print('Secure connection has been closed to the bank server')
+
